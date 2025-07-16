cbf1cfd9ea484c97603c0f6ad2da0a0529b7df9f
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-07-16 14:49:13 -0400

    component notation

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <h1>From App</h1>
-      <NavigationBar />
+      <NavigationBar></NavigationBar>
       <Image></Image>
       <Post></Post>
       <CaptionForm></CaptionForm>
