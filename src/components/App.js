5d6acc1c2f65be9363c5318394ecf2b9fb2fd014
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-07-16 14:26:21 -0400

    fixed src for import

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import '../styles/App.css';
-import { NavigationBar } from 'NavigationBar';
-import { Image } from 'Image';
-import { Post } from 'Post';
-import { CaptionForm } from 'CaptionForm';
+import { NavigationBar } from './NavigationBar';
+import { Image } from './Image';
+import { Post } from './Post';
+import { CaptionForm } from './CaptionForm';
 
 function App() {
   return (
