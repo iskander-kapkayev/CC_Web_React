0b0fdf0505c448014cdf9ba6b53d36a0c8634dd0
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-07-16 14:50:42 -0400

    adjusted CaptionForm component

--- src/components/CaptionForm.js
+++ src/components/CaptionForm.js
@@ -5,11 +5,11 @@ import React from 'react';
 function CaptionForm() {
     return (
 
-        <div className="form-container" id="captionform" style="display: none;">
+        <div className="form-container" id="captionform">
             <h3>Can you make us laugh?</h3> 
             <form id="captionFormData">
-            <input type="caption" id="caption" name="caption" placeholder="Type your caption here..." required> </input>
-            <button type="submit" value="Submit">Add Cap</button>
+                <input type="caption" id="caption" name="caption" placeholder="Type your caption here..." required> </input>
+                <button type="submit" value="Submit">Add Cap</button>
             </form>
         </div>
         
