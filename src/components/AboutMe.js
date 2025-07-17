05fc736917466e410f3a4844c7b996926770fb01
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-07-17 11:24:20 -0400

    added loginform placeholder and adjusted aboutme styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,11 +10,11 @@ function AboutMe() {
                 <h2>About Me:</h2>
 
                 <p id="aboutmetext">
-                    This is a free to use caption contest!<br></br>
-                    Feel free to enjoy it and share the link with others.<br></br><br></br>
-                    In order to participate, you will need to register an account.<br></br>
-                    After you register, you can vote and leave your own caption.<br></br>
-                    <strong>Try to get the most likes for your caption!</strong><br></br><br></br>
+                    This is a free to use caption contest!<br/>
+                    Feel free to enjoy it and share the link with others.<br/><br/>
+                    In order to participate, you will need to register an account.<br/><br/>
+                    After you register, you can vote and leave your own caption.<br/><br/>
+                    <strong>Try to get the most likes for your caption!</strong><br/><br/>
                     
                     All images/gifs are borrowed and are not owned by me.
                 </p>
@@ -23,7 +23,7 @@ function AboutMe() {
             <div className="contactcontainer">
 
                 <p>
-                    In order to contact me, you can email me at:<br></br>
+                    In order to contact me, you can email me at:<br/>
                     iskander.kapkayev@gmail.com
                 </p>
             
