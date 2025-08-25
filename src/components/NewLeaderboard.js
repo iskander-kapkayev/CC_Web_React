87ca8f9d80a831b001ca448eb8b4dae7379d960e
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-08-25 11:32:52 -0400

    created a new leaderboard, to diagnose the issue with the old leaderboard

---
+++ src/components/NewLeaderboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NewLeaderboard() {
+    return (
+
+        <div className="aboutmecontainer">
+
+                <h2>Leaderboard:</h2>
+                <p>Leaderboard is currently loading...</p>
+
+        </div>
+        
+    );
+}
+
+export default NewLeaderboard;
