1c294464380f83c9812a5b3ea3554efcfb644dce
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-08-29 14:32:06 -0400

    images, currentIndex and images[currentIndex] are properly working

--- src/components/Image.js
+++ src/components/Image.js
@@ -74,6 +74,7 @@ function Image() {
     const currentSRC = images[currentIndex];
     const imageButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     console.log(images);
+    console.log(currentIndex);
     console.log(images[currentIndex]);
     return (
         <div className='image-container'>
