431d2797a6e93ea08bb62eef2366e08d61d243eb
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-09-01 23:11:57 -0400

    adjusted caption display

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,6 +8,7 @@ function Post(props) {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [captions, setCaptions] = useState({});
+    const [captionKeys, setCaptionKeys] = useState();
 
     const { currentIndex } = props;
 
@@ -26,6 +27,7 @@ function Post(props) {
                 const captionList = await response.json();
 
                 setCaptions(captionList);
+                setCaptionKeys(Object.keys(captionList));
                 setLoading(false);
                 setError(null);
             } catch (err) {
@@ -38,36 +40,35 @@ function Post(props) {
 
     }, [currentIndex]); // if imageid changes, re-render 
 
-    console.log(captions);
-
     return (
         <div className='image-container'>
             <h2>User Captions:</h2>
             <div className="post-container">
-                {loading ? 
-                    ( <h2>Captions are loading from the server...</h2>) 
-                    : 
-                    (
-                        <div>
-                            <p>nothing to see here</p>
-                            {Object.keys(captions).forEach(captionid => {
-                                (
-                                <div>
-                                    <span id='captuser'>
-                                        <span id='postCaption'>{captions[captionid].captiontext} </span>
-                                        <span id='postUser'> - {captions[captionid].username} #{captions[captionid].category} </span>
+                {loading ? (
+                    <h2>Captions are loading from the server...</h2>
+                ) : (
+                    captionKeys.length === 0 ? (
+                        <p>No captions available.</p>
+                    ) : (
+                        captionKeys.map(captionId => (
+                            <div key={captionId}>
+                                <span id='captuser'>
+                                    <span id='postCaption'>{captions[captionId].captiontext} </span>
+                                    <span id='postUser'> - {captions[captionId].username} #{captions[captionId].category} </span>
+                                </span>
+                                <div id='postUpvotes'>
+                                    <span className='heart'>
+                                        <a><i id='downvoteheart' className="material-symbols-outlined">remove</i></a>
                                     </span>
-                                    <div id='postUpvotes'>
-                                        <span class='heart'> <a><i id='downvoteheart' class="material-symbols-outlined">remove</i></a></span>
-                                        <span class='votenum'>{captions[captionid].votecount}</span>
-                                        <span class='heart'> <a><i id='upvoteheart' class="material-symbols-outlined">add</i></a></span>
-                                    </div>
+                                    <span className='votenum'>{captions[captionId].votecount}</span>
+                                    <span className='heart'>
+                                        <a><i id='upvoteheart' className="material-symbols-outlined">add</i></a>
+                                    </span>
                                 </div>
-                                )
-                            })}
-                        </div>
+                            </div>
+                        ))
                     )
-                }
+                )}
             </div>
         </div>
     );
