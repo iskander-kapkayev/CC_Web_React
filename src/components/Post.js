b8e1d78974b37fb0e110a17c886d11e202a6ae40
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-09-01 23:26:13 -0400

    cleaned up the post code for rendering posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,7 +8,6 @@ function Post(props) {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [captions, setCaptions] = useState({});
-    const [captionKeys, setCaptionKeys] = useState([]);
 
     const { currentIndex } = props;
 
@@ -27,7 +26,6 @@ function Post(props) {
                 const captionList = await response.json();
 
                 setCaptions(captionList);
-                setCaptionKeys(Object.keys(captionList));
                 setLoading(false);
                 setError(null);
             } catch (err) {
@@ -47,28 +45,26 @@ function Post(props) {
                 {loading ? (
                     <h2>Captions are loading from the server...</h2>
                 ) : (
-                    captionKeys.length === 0 ? (
-                        <p>No captions available.</p>
-                    ) : (
-                        captionKeys.map(captionId => (
-                            <div key={captionId} className='post'>
-                                <span id='captuser'>
-                                    <span id='postCaption'>{captions[captionId].captiontext} </span>
-                                    <span id='postUser'> - {captions[captionId].username} #{captions[captionId].category} </span>
+                    Object.keys(captions).map(captionId => 
+                        (
+                        <div key={captionId} className='post'>
+                            <span id='captuser'>
+                                <span id='postCaption'>{captions[captionId].captiontext} </span>
+                                <span id='postUser'> - {captions[captionId].username} #{captions[captionId].category} </span>
+                            </span>
+                            <div id='postUpvotes'>
+                                <span className='heart'>
+                                    <a><i id='downvoteheart' className="material-symbols-outlined">remove</i></a>
                                 </span>
-                                <div id='postUpvotes'>
-                                    <span className='heart'>
-                                        <a><i id='downvoteheart' className="material-symbols-outlined">remove</i></a>
-                                    </span>
-                                    <span className='votenum'>{captions[captionId].votecount}</span>
-                                    <span className='heart'>
-                                        <a><i id='upvoteheart' className="material-symbols-outlined">add</i></a>
-                                    </span>
-                                </div>
+                                <span className='votenum'>{captions[captionId].votecount}</span>
+                                <span className='heart'>
+                                    <a><i id='upvoteheart' className="material-symbols-outlined">add</i></a>
+                                </span>
                             </div>
-                        ))
-                    )
-                )}
+                        </div>
+                        )
+                    ))
+                }
             </div>
         </div>
     );
