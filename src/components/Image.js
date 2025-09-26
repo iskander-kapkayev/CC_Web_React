0670d19e35bbab87f152b8d6d1e77be238d3259b
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-09-26 17:42:01 -0400

    fix on array

--- src/components/Image.js
+++ src/components/Image.js
@@ -57,7 +57,7 @@ function Image(props) {
                 }
             }
 
-            return arrayOfButtons(buttonIndex);
+            return arrayOfButtons[buttonIndex];
         };
 
         /* find category of currentIndex */
