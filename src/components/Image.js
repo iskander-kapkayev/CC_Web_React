71613b07f428cdc38cc16413b139ed23abed2744
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-09-26 16:02:59 -0400

    added different buttons for different categories of captions

--- src/components/Image.js
+++ src/components/Image.js
@@ -88,6 +88,12 @@ function Image(props) {
 
     /* display the current image! */
     const imageButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    const narutoButtons = [1, 2, 3];
+    const onepieceButtons = [];
+    const dbzButtons = [13, 14, 15, 16];
+    const miscButtons = [4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const basketballButtons = [17, 18, 19, 20, 21, 22, 23];
+
 
     return (
         <div className='image-container'>
